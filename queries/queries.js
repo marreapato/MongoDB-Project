75f08ba774154e386b670a6a4203ed8789b5d11c
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-05 19:09:06 -0300

    Update queries.js

--- queries/queries.js
+++ queries/queries.js
@@ -1,7 +1,12 @@
 //usuarios com 1 jogo apenas
 db.usuarios.find({'jogos':{$size:1}}).pretty();
 
+//match - empresas localizadas nos EUA
 
-//jogadores com mais de 27 anos
-db.usuarios.find({ $where: "this.idade > 27" });
+db.empresasProdutorasDeGames.aggregate([{ $match: { paisOrigem: "EUA" } }]);
 
+db.empresasProdutorasDeGames.find();
+
+
+//jogos com preço maior que 20 reais
+db.jogosVideoGame.find({ $where: "this.preco > 20" });

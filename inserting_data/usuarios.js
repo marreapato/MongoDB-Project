188d3a83a71e2642fadbfa4c05e2c3a8d448d236
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-05 17:38:38 -0300

    Update usuarios.js

--- inserting_data/usuarios.js
+++ inserting_data/usuarios.js
@@ -4,7 +4,6 @@ db.usuarios.insertMany([
         "nome": "João Silva",
         "idade": 30,
         "email": "joao.silva@example.com",
-        "endereco": "Rua A, 123, São Paulo, SP",
         "jogos": ["The Last of Us", "Grand Theft Auto V"],
         "acessoAntecipado": true,
         "nickname": "jsilva"
@@ -13,7 +12,6 @@ db.usuarios.insertMany([
         "nome": "Maria Oliveira",
         "idade": 25,
         "email": "maria.oliveira@example.com",
-        "endereco": "Rua B, 456, Rio de Janeiro, RJ",
         "jogos": ["Red Dead Redemption 2", "The Witcher 3: Wild Hunt"],
         "acessoAntecipado": false,
         "nickname": "mariaoliveira"
@@ -22,7 +20,6 @@ db.usuarios.insertMany([
         "nome": "Carlos Souza",
         "idade": 35,
         "email": "carlos.souza@example.com",
-        "endereco": "Rua C, 789, Belo Horizonte, MG",
         "jogos": ["Half-Life 2", "Portal 2", "The Elder Scrolls V: Skyrim"],
         "acessoAntecipado": true,
         "nickname": "csouza"
@@ -31,7 +28,6 @@ db.usuarios.insertMany([
         "nome": "John Doe",
         "idade": 28,
         "email": "johndoe@example.com",
-        "endereco": "Rua D, 321, São Paulo, SP",
         "jogos": ["The Last of Us", "The Last of Us Part II"],
         "acessoAntecipado": false,
         "nickname": "johndoe"
@@ -40,7 +36,6 @@ db.usuarios.insertMany([
         "nome": "Jane Doe",
         "idade": 26,
         "email": "janedoe@example.com",
-        "endereco": "Rua E, 654, Rio de Janeiro, RJ",
         "jogos": ["The Last of Us", "The Last of Us Part II"],
         "acessoAntecipado": true,
         "nickname": "janedoe"
@@ -49,7 +44,6 @@ db.usuarios.insertMany([
         "nome": "Gabriel Ribeiro",
         "idade": 22,
         "email": "gamer123@example.com",
-        "endereco": "Rua F, 987, Belo Horizonte, MG",
         "jogos": ["Grand Theft Auto V", "Grand Theft Auto IV"],
         "acessoAntecipado": false,
         "nickname": "gamer123"
@@ -58,7 +52,6 @@ db.usuarios.insertMany([
         "nome": "Paula Lima",
         "idade": 24,
         "email": "player456@example.com",
-        "endereco": "Rua G, 159, São Paulo, SP",
         "jogos": ["Red Dead Redemption 2"],
         "acessoAntecipado": true,
         "nickname": "player456"
@@ -67,7 +60,6 @@ db.usuarios.insertMany([
         "nome": "Washington Luis",
         "idade": 29,
         "email": "witcherfan@example.com",
-        "endereco": "Rua H, 753, Rio de Janeiro, RJ",
         "jogos": ["The Witcher 3: Wild Hunt"],
         "acessoAntecipado": false,
         "nickname": "witcherfan"
@@ -76,7 +68,6 @@ db.usuarios.insertMany([
         "nome": "Renato Livio",
         "idade": 27,
         "email": "rpglover@example.com",
-        "endereco": "Rua I, 246, Belo Horizonte, MG",
         "jogos": ["The Witcher 3: Wild Hunt"],
         "acessoAntecipado": true,
         "nickname": "rpglover"
@@ -85,7 +76,6 @@ db.usuarios.insertMany([
         "nome": "Paulo Fagundes",
         "idade": 23,
         "email": "portalplayer@example.com",
-        "endereco": "Rua J, 135, São Paulo, SP",
         "jogos": ["Portal 2"],
         "acessoAntecipado": false,
         "nickname": "portalplayer"
@@ -94,7 +84,6 @@ db.usuarios.insertMany([
         "nome": "Fernanda Souza",
         "idade": 32,
         "email": "halflifefan@example.com",
-        "endereco": "Rua K, 864, Rio de Janeiro, RJ",
         "jogos": ["Half-Life 2"],
         "acessoAntecipado": true,
         "nickname": "halflifefan"
@@ -103,7 +92,6 @@ db.usuarios.insertMany([
         "nome": "Cassie Silva",
         "idade": 31,
         "email": "tesfan@example.com",
-        "endereco": "Rua L, 975, Belo Horizonte, MG",
         "jogos": ["The Elder Scrolls V: Skyrim"],
         "acessoAntecipado": false,
         "nickname": "tesfan"
@@ -112,7 +100,6 @@ db.usuarios.insertMany([
         "nome": "Oliver Rivera",
         "idade": 33,
         "email": "morrowindlover@example.com",
-        "endereco": "Rua M, 468, São Paulo, SP",
         "jogos": ["The Elder Scrolls IV: Morrowind"],
         "acessoAntecipado": true,
         "nickname": "morrowindlover"

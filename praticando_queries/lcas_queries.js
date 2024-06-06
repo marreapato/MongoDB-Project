98e2dd6cfba4e9313291bddf5f08eba7babbaf0a
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-06 20:04:30 -0300

    Update lcas_queries.js

--- praticando_queries/lcas_queries.js
+++ praticando_queries/lcas_queries.js
@@ -1,12 +1,26 @@
-//usuarios com 1 jogo apenas
-db.usuarios.find({'jogos':{$size:1}}).pretty();
+use projeto3cin
 
-//match - empresas localizadas nos EUA
+show collections
 
-db.empresasProdutorasDeGames.aggregate([{ $match: { paisOrigem: "EUA" } }]);
+// find e pretty sem o id (procurando jogos de ação sem o id)
 
-db.empresasProdutorasDeGames.find();
+db.jogosVideoGame.find({'genero':'Ação'},{_id:false}).pretty();
 
 
-//jogos com preço maior que 20 reais
-db.jogosVideoGame.find({ $where: "this.preco > 20" });
+// usuarios com 2 jogos e com acesso antecipado a compras
+
+db.usuarios.find({ "jogos": { $size: 2 } , 'acessoAntecipado':true});
+
+//aggregate e match empresas sediadas nos EUA
+
+db.empresasProdutorasDeGames.aggregate([{$match:{"paisOrigem": "EUA"}}]).pretty();
+
+
+//aggregate e match para empresas que foram fundadas apos 1990 ponto 5 da lista
+
+db.empresasProdutorasDeGames.aggregate([{ $match: { "fundacao": { $gte: 1990 } } }]).pretty();
+//equivale 
+
+db.empresasProdutorasDeGames.find({"fundacao":{$gte:1990}}).pretty();
+
+

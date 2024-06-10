b9f29266812b9870c297a9be4e4a7950d17902a9
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-10 18:06:16 -0300

    Update lcas_queries.js

--- praticando_queries/lcas_queries.js
+++ praticando_queries/lcas_queries.js
@@ -262,16 +262,3 @@ db.usuarios.find();
 
 
 
-// lookup para juncao de jogos e empresas
-
-db.empresasProdutorasDeGames.aggregate([{
-  $lookup: {
-    from: "jogosVideoGame",
-    localField: "jogosDesenvolvidos",
-    foreignField: "titulo",
-    as: "jogosDesenvolvidos"
-  }
-},{ $out: "desenvolvedorasJogosPerfis" }]);
-
-db.jogosVideoGame.find()
-db.desenvolvedorasJogosPerfis.find().pretty();

3cc415c103d9fee268453f0002817c8e86af1729
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-11 11:18:05 -0300

    Update queries_documento.js

--- queries_finais/queries_documento.js
+++ queries_finais/queries_documento.js
@@ -1,14 +1,21 @@
-use projeto3cin
-
 use project3cin
 
 show collections
 
 db.desenvolvedorasJogosPerfis.find().pretty();
-//busca em pela palavra jogos nos comentarios das empresas
+
 //ponto 33 - criacao de indice de texto + 34 - uso do search para busca + 2 find + 26 pretty + 16 limit + 39 skip
 
 db.desenvolvedorasJogosPerfis.createIndex({ "comentarios.comentario": "text" });
+db.desenvolvedorasJogosPerfis.find({ $text: { $search: "jogos" } }).count();// total documento que fala de jogos
 db.desenvolvedorasJogosPerfis.find({ $text: { $search: "jogos" } }).limit(1).pretty();// primeiro documento que fala de jogos
 db.desenvolvedorasJogosPerfis.find({ $text: { $search: "jogos" } }).skip(1).pretty();// restante dos documentos
 
+// ponto 4 + ponto 8 + 9 + 13 media das avaliacoes dos sites por genero de jogo
+
+db.desenvolvedorasJogosPerfis.aggregate([{ $unwind: "$jogosDesenvolvidos"},{ $unwind: "$jogosDesenvolvidos.criticas"},
+{$group: {
+      _id: "$jogosDesenvolvidos.genero",
+      avgAvalicao: { $avg: "$jogosDesenvolvidos.criticas.avaliacao" }
+    }}
+]);

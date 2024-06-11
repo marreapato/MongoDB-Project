58ce4d28f47faf40d21521deb921249270c0850d
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-11 10:54:54 -0300

    Update queries_documento.js

--- queries_finais/queries_documento.js
+++ queries_finais/queries_documento.js
@@ -1,1 +1,14 @@
 use projeto3cin
+
+use project3cin
+
+show collections
+
+db.desenvolvedorasJogosPerfis.find().pretty();
+//busca em pela palavra jogos nos comentarios das empresas
+//ponto 33 - criacao de indice de texto + 34 - uso do search para busca + 2 find + 26 pretty + 16 limit + 39 skip
+
+db.desenvolvedorasJogosPerfis.createIndex({ "comentarios.comentario": "text" });
+db.desenvolvedorasJogosPerfis.find({ $text: { $search: "jogos" } }).limit(1).pretty();// primeiro documento que fala de jogos
+db.desenvolvedorasJogosPerfis.find({ $text: { $search: "jogos" } }).skip(1).pretty();// restante dos documentos
+

d68bf45d46fc31d6f1f8ba4cac76171ff9facda4
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-13 15:37:10 -0300

    Update queries_documento.js

--- queries_finais/queries_documento.js
+++ queries_finais/queries_documento.js
@@ -5,8 +5,6 @@ use project3cin
 
 show collections
 
-db.desenvolvedorasJogosPerfis.find().pretty();
-
 //ponto 33 - criacao de indice de texto + 34 - uso do search para busca + 2 find + 26 pretty + 16 limit + 39 skip
 
 db.desenvolvedorasJogosPerfis.createIndex({ "comentarios.comentario": "text" });
@@ -23,13 +21,12 @@ db.desenvolvedorasJogosPerfis.aggregate([{ $unwind: "$jogosDesenvolvidos"},{ $un
 ]);
 
 
-//todos usuarios que jogam the last of us
+//todos usuarios que jogam Grand Theft Auto IV
     
 db.usuarios.find({ "jogos.jogo": { $all: ["Grand Theft Auto IV"]}});
 
 //quantidade de usuarios por jogo    
     
-
 db.usuarios.aggregate([
     {
         $unwind: "$jogos"

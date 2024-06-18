59bc0a977179979bd93d8011b1f3110052305e96
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-06-18 14:23:01 -0300

    Update praticas.js

--- praticando_queries/praticas_para_apresentacao/praticas.js
+++ praticando_queries/praticas_para_apresentacao/praticas.js
@@ -1,2 +1,6 @@
 //Quais são os usuários que têm acesso antecipado aos jogos?
 db.usuarios.aggregate([{$match:{$expr:{$eq:["$acessoAntecipado",true]}}}]);
+
+//Quais são os jogos mais jogados pelos usuários, considerando o percentual de horas jogadas?
+db.usuarios.aggregate([{$unwind:"$jogos"},{$group:{_id:{Jogo:"$jogos.jogo",Horas:"$jogos.percentualHoras"},"total":{$sum:1}}},{$sort:{"total":1}}]);
+//ou
